5f137c04928aa86bcaef55a7ce2beb598e0df6d4
Igor Rybalka <irybalka@yahoo.com>
2025-07-03 15:03:03 +0300

    initial commit

---
+++ bin/dart_collections_journeydart.dart
@@ -0,0 +1,5 @@
+import 'package:dart_collections_journeydart/dart_collections_journeydart.dart' as dart_collections_journeydart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_collections_journeydart.calculate()}!');
+}
---
+++ test/dart_collections_journeydart_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_collections_journeydart/dart_collections_journeydart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
